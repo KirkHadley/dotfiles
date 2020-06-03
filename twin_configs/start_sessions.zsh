9a4590b93ba46bcaad8d8653260868e40b0286ce
Kirk Hadley <fhadley0411@gmail.com>
2020-06-03 11:43:48 -0400

    updated

--- twin_configs/start_sessions.zsh
+++ twin_configs/start_sessions.zsh
@@ -2,12 +2,15 @@
 source ~/.bash_aliases
 mldevpublicip=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=Kirk" --query "Reservations[*].Instances[*].PublicIpAddress"  --output=text)
 eastmlsinglegpu=$(aws ec2 describe-instances --region us-east-1 --filters Name=tag:Name,Values=ml-dev-gpu-base --query "Reservations[*].Instances[*].PublicIpAddress" --output=text)
-westmlsinglegpu=$(aws ec2 describe-instances --region us-west-2 --filters Name=tag:Name,Values=mldevgpu --query "Reservations[*].Instances[*].PublicIpAddress" --output=text)
+westmlsinglegpu1=$(aws ec2 describe-instances --region us-west-2 --filters Name=tag:Name,Values=mldevgpu1 --query "Reservations[*].Instances[*].PublicIpAddress" --output=text)
+westmlsinglegpu2=$(aws ec2 describe-instances --region us-west-2 --filters Name=tag:Name,Values=mldevgpu2 --query "Reservations[*].Instances[*].PublicIpAddress" --output=text)
 
 export mldevpublicip
 export eastmlsinglegpu
-export westmlsinglegpu 
-for session in mldev ipy singlegpu multigpu jpy wgpu
+export westmlsinglegpu1
+export westmlsinglegpu2
+
+for session in mldev ipy singlegpu multigpu jpy w1gpu w2gpu
 do
 tmux has-session -t $session 2>/dev/null
 if [ $? != 0 ]; then
@@ -29,8 +32,12 @@ if [ $? != 0 ]; then
     tmux new-session -d -s $session "ssh -i ~/Downloads/tdaws.pem ubuntu@$eastmlsinglegpu"
     # tmux send-keys -t $session "tma bp" ENTER
   fi
-  if [ $session = 'wgpu' ]; then
-    tmux new-session -d -s $session "ssh -i ~/Downloads/tdaws.pem ubuntu@$westmlsinglegpu"
+  if [ $session = 'w1gpu' ]; then
+    tmux new-session -d -s $session "ssh -i ~/Downloads/tdaws.pem ubuntu@$westmlsinglegpu1"
+    # tmux send-keys -t $session "tma bp" ENTER
+  fi
+  if [ $session = 'w2gpu' ]; then
+    tmux new-session -d -s $session "ssh -i ~/Downloads/tdaws.pem ubuntu@$westmlsinglegpu2"
     # tmux send-keys -t $session "tma bp" ENTER
   fi
   if [ $session = 'jpy' ]; then 
